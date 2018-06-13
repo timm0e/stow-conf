64fbb8d35a96a09946831af683e8deda5cb69be2
Maxr1998 <max.rumpf1998@gmail.com>
2018-06-13 19:48:41 +0200

    More feature for setup script, update modules

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/fish
+
+echo "Refreshing submodules"
+git submodule init
+git submodule update --init --recursive --remote
+
+
+which pacman > /dev/null
+if test $status -eq 0
+    echo "Arch Linux detected, installing packages if required.."
+    sudo pacman -S --needed fish gnu-netcat thefuck
+end
+
+echo "Checking for Vundle"
+if not test -d ~/.vim/bundle/Vundle.vim
+    echo "Installing.."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+end
+
 if count $argv > /dev/null
    and [ $argv[1] = "teardown" ]
     for pkg in *
